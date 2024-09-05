2b3ab3e69c80894bdc6525207ffbd8d541638c1d
Comfort99 <shingangecomfort@gmail.com>
2024-09-05 13:51:56 +0200

    autharization paths

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+""" Authentication Module """
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth:
+    """ Class to manage the API authentication """
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """ Determines if authentication is required
+        based on the path and excluded paths.
+
+        Args:
+            path (str): The path to be checked.
+            excluded_paths (List[str]):
+            A list of paths that do not require authentication.
+        Returns:
+            bool"""
+        if path is None:
+            return True
+        if not excluded_paths:
+            return True
+
+        path = path if path.endswith('/') else path + '/'
+
+        for excluded_path in excluded_paths:
+            if excluded_path == path:
+                return False
+
+        return True
+
+    def authorization_header(self, request=None) -> str:
+        """ Retrieves the authorization header from the request.
+
+        Args:
+            request: The Flask request object.
+
+        Returns:
+            str: None, since this will be implemented later. """
+        if request is None:
+            return None
+
+        return request.headers.get("Authorization", None)
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """ Retrieves the current user based on the request.
+
+        Args:
+            request: The Flask request object."""
+        return None
